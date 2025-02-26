3fb91d64be5e60afbf0bc7d6312e4023e21a7323
Guilherme-in-Amaro <guiletsgo@gmail.com>
2025-02-26 20:13:13 -0300

    Calculadora GuilhermeAmaro

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numero1, numero2;
+
+string operacao;
+
+
+Console.WriteLine("digite o primeiro numero");
+numero1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("escreva o segundo numero");
+numero2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("digite a operação");
+operacao = Console.ReadLine();
+
+if (operacao == "+")
+{
+    Console.WriteLine(numero1 + numero2);
+}
+if (operacao == "-")
+{
+    Console.WriteLine(numero1 - numero2);
+}
+if (operacao == "*")
+{ Console.WriteLine(numero1 * numero2); }
+
+if (numero2 != 0)
+{ Console.WriteLine(numero1 / numero2); }
+
+else
+{ Console.WriteLine("O numero indicado nao pode ser dividido por zero"); }
+
